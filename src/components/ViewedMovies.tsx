dd5c8821758da70326b193bc7ad0d72e14739012
bit_guber <mgrguber@gmail.com>
2023-10-19 20:23:48 +0530

    update view event

--- src/components/ViewedMovies.tsx
+++ src/components/ViewedMovies.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import options from "./helper"
 import "./inner.css"
 import MovieCard from "./MovieCard";
@@ -5,8 +6,12 @@ import MovieCard from "./MovieCard";
 
 // const poster = 'https://m.media-amazon.com/images/M/MV5BMDU2ZWJlMjktMTRhMy00ZTA5LWEzNDgtYmNmZTEwZTViZWJkXkEyXkFqcGdeQXVyNDQ2OTk4MzI@._V1_QL75_UX380_CR0,2,380,562_.jpg'
 
-let viewedMoiveList = options.viewedMovieList;
+
 function ViewedMovies(){
+    let [viewedMoiveList , setMovieList] = useState(options.viewedMovieList);
+    useEffect( ()=>{
+        setMovieList( viewedMoiveList );
+    }, [ viewedMoiveList ] );
     return <div className="ViGrid">
         { viewedMoiveList.map( (object, index)=> <MovieCard movie_id={object} viewed={true} key={index} /> ) }
     </div>
