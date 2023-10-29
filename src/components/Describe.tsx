a1c218d95c68b156321b0c18c176965633b5f687
bit_guber <mgrguber@gmail.com>
2023-10-29 16:06:31 +0530

    mobile compadiablity v5

--- src/components/Describe.tsx
+++ src/components/Describe.tsx
@@ -2,8 +2,8 @@
 function Describe(){
     return <div className="aboutpage">
                 <h1>Movies suggestion</h1>
-                <p>Disclaimer: this portfolio project support 10 Liked movies by <mark>bit_guber</mark>, you can find more information <a href="https://github.com/bit-guber/Portfolio">here</a><br>
-                    </br>here you can select Movies which you liked movie and site Recommendation System will find top 50 good matches. Initially Recommendation list from Most Popular Movies in MovieLens <a href="https://grouplens.org/datasets/movielens/">dataset</a>. 
+                <p>Disclaimer: this portfolio project support 10 Liked movies by <mark>bit_guber</mark>, you can find more information <a href="https://github.com/bit-guber/Portfolio" target="_blank" rel="noopener noreferrer">here</a><br></br>
+                    here you can select Movies which you liked movie and site Recommendation System will find top 50 good matches. Initially Recommendation list from Most Popular Movies in MovieLens <a href="https://grouplens.org/datasets/movielens/" target="_blank" rel="noopener noreferrer">dataset</a>. 
                 </p>
            </div>
 }
