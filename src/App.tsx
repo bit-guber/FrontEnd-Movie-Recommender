5e4ac66c62e2a746873164b011591dc46e5d90b8
guber <72848937+bit-guber@users.noreply.github.com>
2023-11-03 08:30:49 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // import options from "./helper"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Describe from "./components/Describe";
 import RecommendedMovies from "./components/RecommendedMovies";
 import ViewedMovies from "./components/ViewedMovies";
