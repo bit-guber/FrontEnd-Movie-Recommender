4cc6d6e41930d30c68465557bb2f7a7c49c142ef
guber <72848937+bit-guber@users.noreply.github.com>
2023-11-03 08:31:47 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,23 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './components/inner.css';
-import options from './components/helper.tsx';
 
-let body_section = { 
-  method: "GET",    
-  headers: {
-      'Content-Type': 'application/json',
-      'accept': 'application/json'
-  },
-  priority: "high"
-}
-fetch(options.api_url + "/get_auth", body_section )
-  .then(Response=>Response.json())
-  .then(response => {
-          options.auth_headers.headers.Authorization+=response.token;
-      }
-  )
-  .catch( error=> console.log("problem lodding api token", error) )
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
