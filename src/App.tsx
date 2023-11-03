93ed2b296f11fa7e6876b3c1aca1faf27888a505
guber <72848937+bit-guber@users.noreply.github.com>
2023-11-03 09:39:04 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -24,25 +24,7 @@ function App() {
     
     setchangestate(!changestate);
   }
-  useEffect( ()=>{
 
-  let body_section = { 
-    method: "GET",    
-    headers: {
-        'Content-Type': 'application/json',
-        'accept': 'application/json'
-    },
-    priority: "high"
-  }
-  fetch(options.api_url + "/get_auth", body_section )
-    .then(Response=>Response.json())
-    .then(response => {
-            options.auth_headers.headers.Authorization+=response.token;
-            console.log( "update auth", options.auth_headers.headers.Authorization );
-        }
-    )
-    .catch( error=> console.log("problem lodding api token", error) )
-  } );
   return (
     <>
       <Describe data-bs-theme = "dark"/>
